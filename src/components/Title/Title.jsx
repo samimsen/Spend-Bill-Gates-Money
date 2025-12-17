6fed706f02680b324dea6955172dda53edc222a0
Samim Sen <samim.sen@hotmail.com>
2025-12-17 22:01:49 +0300

    title img upload

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,7 +3,7 @@ import './Title.css'
 const Title = () => {
     return (
         <div className='title-container'>
-            <img className='title-pic' src={`/img/billgates.jpg}`} alt="Bill Gates" />
+            <img className='title-pic' src="/img/billgates.jpg" alt="Bill Gates" />
             <h1>Spend Bill Gates' Money</h1>
         </div>
     )
